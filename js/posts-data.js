27cedb1885d074678a24fa2e8ffee3e07d488de0
huihui <113282001+chiffon-0504@users.noreply.github.com>
2026-05-03 19:34:06 +0800

    add shared posts data

---
+++ js/posts-data.js
@@ -0,0 +1,19 @@
+const HUIHUI_POSTS = [
+  {
+    authorName: "huihui",
+    authorHandle: "@huihui",
+    date: "Apr 19 2026",
+    content: `Cyaegha <span class="future">FUTURE 10+</span> <span class="ex">EX+</span> !!!!!\n\n<a class="hashtag" href="https://x.com/hashtag/arcaea?src=hashtag_click" target="_blank" rel="noopener noreferrer">#arcaea</a>`,
+    image: {
+      src: "/images/3002_p.webp",
+      altKey: "posts.scoreAlt",
+      caption: "Arcaea images and properties belong to © lowiro."
+    }
+  },
+  {
+    authorName: "huihui",
+    authorHandle: "@huihui",
+    date: "Apr 14 2026",
+    content: "Hello, World!"
+  }
+];
